e06dceaae2129a0bcc733e345e25da0f461916a3
Elizabeth Kezia <91784486+ekkezia@users.noreply.github.com>
2026-02-24 12:02:53 -0500

    update

---
+++ wk2/magic_wand_mqtt/arduino_secrets.h
@@ -0,0 +1,6 @@
+#define SECRET_SSID "sandbox370"
+#define SECRET_PASS "+s0a+s03!2gether?"
+#define SERVER_IP_ADDRESS "10.23.10.70"
+
+#define SECRET_MQTT_USER "public" // broker username for shiftr.io
+#define SECRET_MQTT_PASS "public" // brsanzoker password for shiftr.io
