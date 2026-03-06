8bd29817766b1526a37aea895ff23518d6a3dc4b
Elizabeth Kezia <91784486+ekkezia@users.noreply.github.com>
2026-03-06 17:51:47 -0500

    production server url

--- wk2/dashboard/src/config.js
+++ wk2/dashboard/src/config.js
@@ -1,2 +1,2 @@
-// export const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL || 'https://imu-remote.onrender.com'; 
-export const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:4000'; 
+export const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL || 'https://imu-remote.onrender.com'; 
+// export const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:4000'; 
